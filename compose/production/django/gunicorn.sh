f54d4b4be4fc7bbaa77fbfbe3bd2c259bb5467a8
Laury Bueno <laury@hacklab.com.br>
2017-09-15 12:35:42 -0300

    [Prod] Database migration is now mandatory on deploy

--- compose/production/django/gunicorn.sh
+++ compose/production/django/gunicorn.sh
@@ -6,6 +6,9 @@ set -o nounset
 
 
 python /app/manage.py collectstatic --noinput
+
+python /app/manage.py migrate
+
 /usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app \
     --error-logfile=- \
     --access-logfile=- \
