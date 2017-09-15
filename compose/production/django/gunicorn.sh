c7f719f56263a9734c29aa32841ede0611803c9e
Laury Bueno <laury@hacklab.com.br>
2017-09-15 16:38:25 -0300

    CI added

--- compose/production/django/gunicorn.sh
+++ compose/production/django/gunicorn.sh
@@ -4,6 +4,12 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+# the official postgres image uses 'postgres' as default user if not set explictly.
+if [ -z "$POSTGRES_USER" ]; then
+    export POSTGRES_USER=postgres
+fi
+
+export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_USER
 
 python /app/manage.py collectstatic --noinput
 
