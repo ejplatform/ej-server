58263d9cf868645c39bfcb8216d01f807258c72d
Marco Silva <marcowilliamdf@gmail.com>
2017-10-24 19:12:36 -0200

    Remove api versioning

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,9 @@ urlpatterns = [
 
     url(r'^api/docs/', include_docs_urls(title='pushtogether API Docs', public=False)),
     # User management
-    # url(r'^users/', include('pushtogether.users.urls', namespace='users')),
+    url(r'^api/users/', include('pushtogether.users.urls', namespace='users')),
     url(r'^polis/', include('pushtogether.polis.urls', namespace='polis')),
-    url(r'^api/v1/', include('pushtogether.conversations.urls', namespace='v1')),
-    url(r'^api/v1/users/', include('pushtogether.users.urls', namespace='v1')),
+    url(r'^api/', include('pushtogether.conversations.urls', namespace='v1')),
     url(r'^rest-auth/', include('rest_auth.urls')),
     # TODO: Remove this redirect after october 2017
     url(r'^rest-auth/registration/account-confirm-email/(?P<key>[-:\w]+)/$', RedirectView.as_view(pattern_name='account_confirm_email'), name='account-confirm-email-redirect'),
