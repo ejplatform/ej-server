35f08e8fdc0a58c1df522111a3ed736b3efcbc55
Marco Silva <marcowilliamdf@gmail.com>
2017-10-27 16:14:44 -0200

    Create endpoint for uploading user image

--- pushtogether/users/views.py
+++ pushtogether/users/views.py
@@ -1,4 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import detail_route, parser_classes
+from rest_framework.response import Response
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework import status
 
 from django.core.urlresolvers import reverse
 from django.views.generic import DetailView, ListView, RedirectView, UpdateView
@@ -21,6 +25,22 @@ from .serializers import UserSerializer
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    @detail_route(methods=['POST'])
+    @parser_classes((FormParser, MultiPartParser))
+    def image(self, request, *args, **kwargs):
+        if 'image' in request.data:
+            user_profile = self.get_object()
+            user_profile.image.delete()
+
+            upload = request.data['image']
+
+            user_profile.image.save(upload.name, upload)
+
+            return Response(status=status.HTTP_201_CREATED,
+                            headers={'Location': user_profile.image.url})
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginSignupView(SignupView):
