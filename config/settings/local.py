1a0298077b030e75ec3f9e04a569b8b02ce3781c
Bruno Martin <bruno.martin@hacklab.com.br>
2017-10-30 19:29:08 -0200

    add allow all to cors when in local environment

--- config/settings/local.py
+++ config/settings/local.py
@@ -78,5 +78,6 @@ INSTALLED_APPS += ['django_extensions', ]
 # ------------------------------------------------------------------------------
 TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 
-# Your local stuff: Below this line define 3rd party library settings
+# Django cors - allow requests from any origin in local environment
 # ------------------------------------------------------------------------------
+CORS_ORIGIN_ALLOW_ALL = True
