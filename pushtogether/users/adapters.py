3855bce72be2eec8e4472f578fa955e5cf714c37
Bruno Martin <bruno.martin@hacklab.com.br>
2017-11-08 17:57:04 -0200

    remove unecessary code

--- pushtogether/users/adapters.py
+++ pushtogether/users/adapters.py
@@ -3,14 +3,7 @@ from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 
-class AccountAdapter(DefaultAccountAdapter):
-    def is_open_for_signup(self, request):
-        return getattr(settings, 'ACCOUNT_ALLOW_REGISTRATION', True)
-
-
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
-    def is_open_for_signup(self, request, sociallogin):
-        return getattr(settings, 'ACCOUNT_ALLOW_REGISTRATION', True)
 
     def populate_user(self, request, sociallogin, data):
         user = super().populate_user(request, sociallogin, data)
