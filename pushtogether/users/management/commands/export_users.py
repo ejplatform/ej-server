d120c0511cc216d510d82264c3a0e0ee25bf67b8
Laury Bueno <laury@hacklab.com.br>
2017-12-12 10:42:01 -0200

    [Command] More infos added to 'export_users'

--- pushtogether/users/management/commands/export_users.py
+++ pushtogether/users/management/commands/export_users.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
     def handle(self, *files, **options):
         with open('users.csv', 'w') as csv:
-            csv.write('id, name, username, email,\n')
+            csv.write('id, name, username, email, last_login,\n')
             for user in User.objects.all():
-                d = '{}, {}, {},\n'.format(str(user.id), str(user.name), str(user.username), str(user.email))
+                d = '{}, {}, {}, {}, {},\n'.format(str(user.id), str(user.name), str(user.username), str(user.email), str(user.last_login))
                 csv.write(d)
