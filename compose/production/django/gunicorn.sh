a4f21b215e432fa66f6ef006f126058e740ee4e0
Laury Bueno <laury@hacklab.com.br>
2018-02-05 06:32:46 -0200

    [Prod] Django and Nginx are now part of a multi stage build

--- compose/production/django/gunicorn.sh
+++ compose/production/django/gunicorn.sh
@@ -4,8 +4,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-python /app/manage.py collectstatic --noinput
-
 python /app/manage.py migrate
 
 /usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app \
