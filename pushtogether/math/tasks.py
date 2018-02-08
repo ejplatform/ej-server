4916dbed8500d6d2f381c474ca4502d060b7cc58
Luan Guimarães <luang@protonmail.ch>
2018-02-08 02:24:04 -0200

    [Math] Configures STUCKED status logic to the CLUSTERS task

--- pushtogether/math/tasks.py
+++ pushtogether/math/tasks.py
@@ -4,12 +4,14 @@ from celery import shared_task
 
 from .models import Job
 from .celeryconf import app
-from ..conversations.models import Conversation
+from pushtogether.conversations.models import Conversation
 
 
 # decorator to avoid code duplication
 def update_job(fn):
-    """Decorator that will update Job with result of the function"""
+    """
+    Decorator that will update Job with result of the function
+    """
 
     # wraps will make the name and docstring of fn available for introspection
     @wraps(fn)
@@ -21,7 +23,7 @@ def update_job(fn):
             # execute the function fn
             result = fn(*args, **kwargs)
             job.result = result
-            job.status = Job.FINISHED
+            job.status = Job.FINISHED if result else Job.STUCKED
             job.save()
         except:
             job.result = None
@@ -29,13 +31,30 @@ def update_job(fn):
         job.save()
     return wrapper
 
-
 @shared_task
 @update_job
 def get_clusters(conversation_id):
+    """
+    Celery task that receives an conversation ID, checks if this conversation
+    has sufficient data to be clustered, than returns the clusters if it's ok,
+    or None if it isn't.
+    """
     conversation = Conversation.objects.get(pk=conversation_id)
-    votes = conversation.list_votes()
-    return cluster.get_clusters(votes, range(2,5))
+    if has_sufficient_data(conversation):
+        votes = conversation.list_votes()
+        return cluster.get_clusters(votes, range(2,5))
+
+
+def has_sufficient_data(conversation):
+    """
+    Check if the conversation available data respect the limits of the
+    math configuration variables.
+    """
+    if (conversation.total_participants < Job.MATH_MIN_USERS or
+        conversation.total_comments < Job.MATH_MIN_COMMENTS or
+        conversation.total_votes < Job.MATH_MIN_VOTES):
+        return False
+    return True
 
 
 TASK_MAPPING = {
