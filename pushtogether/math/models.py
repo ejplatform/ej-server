ac27dd66ae0bc7c0db42a8c74c5af32ae380ec5c
Luan Guimarães <luang@protonmail.ch>
2018-02-08 02:12:02 -0200

    [MATH] Add STUCK status to the Jobs
    now we can save jobs in the sync mode passing the delay=False param

--- pushtogether/math/models.py
+++ pushtogether/math/models.py
@@ -2,7 +2,7 @@ from django.db import models, transaction
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.postgres.fields import JSONField, ArrayField
 
-from ..conversations.models import Conversation
+from pushtogether.conversations.models import Conversation
 
 
 class Job(models.Model):
@@ -19,11 +19,13 @@ class Job(models.Model):
     STARTED = 'STARTED'
     FINISHED = 'FINISHED'
     FAILED = 'FAILED'
+    STUCKED = 'STUCKED'
     STATUS_CHOICES = (
         (PENDING, _('PENDING')),
         (STARTED, _('STARTED')),
         (FINISHED, _('FINISHED')),
         (FAILED, _('FAILED')),
+        (STUCKED, _('STUCKED')),
     )
 
     conversation = models.ForeignKey(Conversation, related_name="math_jobs")
@@ -38,19 +40,28 @@ class Job(models.Model):
     updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
     result = JSONField(null=True)
 
-    def save(self, *args, **kwargs):
-        """Save model and if job is in pending state, schedule it"""
+    # Clustering configuration
+    MATH_MIN_USERS = getattr(settings, 'MATH_MIN_USERS', 5)
+    MATH_MIN_COMMENTS = getattr(settings, 'MATH_MIN_COMMENTS', 5)
+    MATH_MIN_VOTES = getattr(settings, 'MATH_MIN_VOTES', 5)
+
+    def save(self, delay=True, *args, **kwargs):
+        """
+        Save model and if job is in pending state, schedule it
+        """
         super(Job, self).save(*args, **kwargs)
         if self.status == self.PENDING:
             from .tasks import TASK_MAPPING
             task = TASK_MAPPING[self.type]
-
-            # Avoids the concurrency between celery worker and django core
-            # when the core didn't save the Job into the database and the
-            # worker tries to access it to change its state.
-            transaction.on_commit(
-                lambda: task.delay(
-                    job_id=self.id,
-                    conversation_id=self.conversation.id
-                )
-            )
+            kwargs = {
+                'job_id': self.id,
+                'conversation_id': self.conversation.id
+            }
+            if delay:
+                # Avoids the concurrency between celery worker and django core
+                # when the core didn't save the Job into the database and the
+                # worker tries to access it to change its state.
+                execute_task = lambda: task.delay(**kwargs)
+                transaction.on_commit(execute_task)
+            else:
+                task.apply(kwargs=kwargs)
