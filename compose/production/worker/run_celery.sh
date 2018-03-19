3c4e59d42fd1b587056b125ad81d60af80655bfa
Jonathan Moraes <arkyebr@gmail.com>
2018-03-19 12:20:52 -0300

    [Docker] Update compose structure

---
+++ compose/production/worker/run_celery.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# wait for RabbitMQ server to start
+sleep 10
+
+# run Celery worker for our project myproject with Celery configuration stored in Celeryconf
+cd /app
+celery worker -A pushtogether.math -l info 
