18a1f538636a3e6fad3fa1548a7e94fc823214ed
Luan Guimarães <luang@riseup.net>
2018-04-11 22:42:53 -0300

    [RocketChat] Migrate app name to ejrocketchat

--- config/settings/base.py
+++ config/settings/base.py
@@ -65,7 +65,7 @@ INSTALLED_APPS = [
     'ej.conversations.apps.ConversationsConfig',
     'ej.gamification.apps.GamificationConfig',
     'ej.math.apps.MathConfig',
-    'ej.rocketchat.apps.RocketchatConfig',
+    'ej.rocketchat.apps.EJRocketChatConfig',
 ]
 
 # MIDDLEWARE CONFIGURATION
