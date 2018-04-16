a4f8111510aecbccebe0afdbecef60fe8d16185c
pablodiegoss <pablodiegoss@hotmail.com>
2018-04-16 15:02:44 -0300

    Remove commented urls in gamification

--- ej/gamification/urls.py
+++ ej/gamification/urls.py
@@ -2,17 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
-
-# router.register('points-leaderboard', views.PointsLeaderBoardView.as_view(), base_name='points-leaderboard')
-
-# urlpatterns = router.urls
-
 urlpatterns = [
-    # url(
-    #     regex=r'^badges/$',
-    #     view=views.BadgeViewSet.as_view(),
-    #     name='badges-list'
-    # ),
     url(
         regex=r'^points-leaderboard/$',
         view=views.PointsLeaderBoardView.as_view(),
