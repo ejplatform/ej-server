42fab1b2c4fe7f44e0c0c7b4999916bc76327eba
pablodiegoss <pablodiegoss@hotmail.com>
2018-04-17 10:19:31 -0300

    Remove unused user MeViewSet

--- ej/users/api_views.py
+++ ej/users/api_views.py
@@ -46,21 +46,3 @@ class UserViewSet(ModelViewSet):
         elif self.action == 'retrieve':
             self.permission_classes = [IsCurrentUserOrAdmin]
         return super(self.__class__, self).get_permissions()
-
-
-class MeViewSet(ModelViewSet):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
-
-    def retrieve(self, request, pk=None, *args, **kwargs):
-        # Using the /users/me endpoint
-        if pk is None:
-            print('ENTROUOOOOOOOOOOOIUOUO\n\n\n')
-            if self.request.user.has_real_login():
-                instance = request.user
-            else:
-                raise Http404
-        else:
-            instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
