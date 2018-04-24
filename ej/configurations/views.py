74ae79c4ab2a166d48da560f0dd1990395a73fb4
rodrigocam <rodrigo.redcode@gmail.com>
2018-04-24 16:55:16 -0300

    Change get_fragment method

--- ej/configurations/views.py
+++ ej/configurations/views.py
@@ -6,11 +6,11 @@ def get_fragment(name):
     """
     Return a fragment with the given name
     """
-    try:
-        return DEFAULT_FRAGMENTS[name]
-    except KeyError:
-        custom_fragment = Fragment.objects.filter(name=name)
-        if custom_fragment:
-            return custom_fragment
-        else:
+    custom_fragment = Fragment.objects.filter(name=name)
+    if custom_fragment:
+        return custom_fragment
+    else:
+        try:
+            return DEFAULT_FRAGMENTS[name]
+        except KeyError:
             raise ValueError(f'Invalid fragment name "{name}"!')    
