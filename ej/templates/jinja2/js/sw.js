a6279fb810b84c0367abcc72bca335deeb5fcbfa
fabiomacedomendes <fabiomacedomendes@gmail.com>
2018-04-25 11:09:52 -0300

    install service workers correctly

---
+++ ej/templates/jinja2/js/sw.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// Start workbox
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+var log = console.log;
+
+/**
+ * Initialize service workers
+ */
+
+function registerAllRoutes(workbox) {
+    log('[sw] Starting service worker');
+
+    // Cache javascript
+    workbox.routing.registerRoute(
+        new RegExp('.*\.js'),
+        // workbox.strategies.cacheFirst()
+        workbox.strategies.networkFirst()
+    );
+
+    // Css can be safely hot-reloaded on page
+    workbox.routing.registerRoute(
+        new RegExp('.*\.css'),
+        workbox.strategies.staleWhileRevalidate()
+    );
+
+    // Static large immutable binary assets
+    workbox.routing.registerRoute(
+        new RegExp('.*\.(?:png|jpg|jpeg|svg|gif)'),
+        workbox.strategies.cacheFirst({
+            plugins: [
+                new workbox.expiration.Plugin({
+                    maxEntries: 50,
+                    maxAgeSeconds: 7 * 24 * 60 * 60 // a week
+                })
+            ]
+        })
+    );
+
+    // Dynamic urls and root page
+    workbox.routing.registerRoute(
+        new RegExp('[a-zA-Z0-9-\/]*'),
+        workbox.strategies.networkFirst()
+    );
+
+    log('[sw] all routes registered!');
+}
+registerAllRoutes(workbox);
