c1f6c20077c717b6f84741879cd403dae5174d88
fabiomacedomendes <fabiomacedomendes@gmail.com>
2018-04-25 12:32:51 -0300

    harden templates against the BREACH attack

--- compose/production/django/gunicorn.sh
+++ compose/production/django/gunicorn.sh
@@ -5,9 +5,9 @@ set -o pipefail
 set -o nounset
 
 python /app/manage.py migrate
-inv db-assets
-inv db-fake --safe
-echo 'production' >> /app/local/build.info
+# inv db-assets
+# inv db-fake --safe
+# echo 'production' > /app/local/build.info
 
 /usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app \
     --error-logfile=- \
