dd50975c89491c6ac57a8a7cb8266e23c62ac884
rodrigocam <rodrigo.redcode@gmail.com>
2018-04-26 12:42:02 -0300

    Change method get_fragment with new default fragment logic

--- ej/configurations/views.py
+++ ej/configurations/views.py
@@ -1,16 +1,12 @@
 from .models import Fragment
-from .fragments import DEFAULT_FRAGMENTS
+from .fragments import default_fragment
 
 
 def get_fragment(name):
     """
     Return a fragment with the given name
     """
-    custom_fragment = Fragment.objects.filter(name=name)
-    if custom_fragment:
-        return custom_fragment
-    else:
-        try:
-            return DEFAULT_FRAGMENTS[name]
-        except KeyError:
-            raise ValueError(f'Invalid fragment name "{name}"!')    
+    try:
+        return Fragment.objects.get(name=name)
+    except:
+        return default_fragment(name)
