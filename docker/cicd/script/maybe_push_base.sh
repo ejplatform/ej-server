20b0e99c897f4e0a0cdd7822c3d09b1b09450282
Jonathan Moraes <arkyebr@gmail.com>
2018-05-03 22:44:07 -0300

    [Docker] Improve CICD update base images script
    build image <base>
    
    update image <base>

--- docker/cicd/script/maybe_push_base.sh
+++ docker/cicd/script/maybe_push_base.sh
@@ -2,7 +2,7 @@
 
 printf "\n## Updating Base Images\n\n"
 
-if test -n "${git show | grep "update image <base>"}"; then
+if [ -n "`git show | grep "update image <base>"`" ]; then
   printf "\nPerforming Login on Docker.Hub:\n\n"
   docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASS
   printf "\nPushing Base Image:\n\n"
@@ -11,12 +11,12 @@ if test -n "${git show | grep "update image <base>"}"; then
   docker-compose -f ./docker/base/test.build.yml push
   printf "\nPushing Production Image:\n\n"
   docker-compose -f ./docker/base/production.build.yml push
-elif test -n "${git show | grep "update image <base-test>"}"; then
+elif [ -n "`git show | grep "update image <base-test>"`" ]; then
   printf "\nPerforming Login on Docker.Hub:\n\n"
   docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASS
   printf "\nPushing Test Image:\n\n"
   docker-compose -f ./docker/base/test.build.yml push
-elif test -n "${git show | grep "update image <base-production>"}"; then
+elif [ -n "`git show | grep "update image <base-production>"`" ]; then
   printf "\nPerforming Login on Docker.Hub:\n\n"
   docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASS
   printf "\nPushing Production Image:\n\n"
