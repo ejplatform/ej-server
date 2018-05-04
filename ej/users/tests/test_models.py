7176c35c17715f62542f2c7893672e3730cd2c89
pablodiegoss <pablodiegoss@hotmail.com>
2018-05-04 16:47:58 -0300

    Test UserManager model

--- ej/users/tests/test_models.py
+++ ej/users/tests/test_models.py
@@ -1,5 +1,5 @@
 from test_plus.test import TestCase
-
+from ..models import UserManager, User
 
 class TestUser(TestCase):
 
@@ -17,3 +17,92 @@ class TestUser(TestCase):
             self.user.get_absolute_url(),
             '/api/v1/users/{}/'.format(self.user.id)
         )
+
+class TestUserManager(TestCase):
+    def setUp(self):
+        self.user_manager = UserManager()
+        self.user_manager.model = User
+        self.user_manager.create_user(username="asd", email="fake@to.populate", password="abcd")
+        self.user_manager.create_user(username="ej", email="empurrando@email.com", password="123")
+        self.user_manager.create_user(username="asd2", email="fake2@to.populate", password="abcd")
+
+    def test_get_by_email_or_username(self):
+        #Find by username
+        user = self.user_manager.get_by_email_or_username("ej")
+        assert user.email == "empurrando@email.com"
+        assert user.username == "ej"
+
+        #Find by email
+        user = self.user_manager.get_by_email_or_username("empurrando@email.com")
+        assert user.username == "ej"
+        assert user.email == "empurrando@email.com"
+
+        #Raise error if not found
+        try:
+            self.user_manager.get_by_email_or_username("do not exist")
+            assert False
+        except:
+            assert True
+
+    def test_create_simple_user(self):
+        user = self.user_manager.create_simple_user("ej2", "empurrando2@email.com", "123123")
+
+        # Test user created
+        assert user.__class__ == User
+        assert user.name == "ej2"
+        assert user.email == "empurrando2@email.com"
+        assert self.user_manager.get_by_email_or_username("empurrando2@email.com") == user
+
+        # Email that is already used should raise exception
+        try:
+            self.user_manager.create_simple_user("ej2", "empurrando2@email.com", "123123")
+            assert False
+        except:
+            assert True
+
+    def test_available_user_name(self):
+
+        # Test Available Name combinations based on email and name
+        username = self.user_manager.available_username("empurrando juntos", "empurrando2@email.com")
+        #Email name
+        assert username == "empurrando2"
+
+        self.user_manager.create_user(username="empurrando2", email="empurrando2@email.com", password="abcd")
+        username = self.user_manager.available_username("empurrando juntos", "empurrando2@email.com")
+        #First name
+        assert username == "empurrando"
+
+        self.user_manager.create_user(username="empurrando", email="empurrando2@email.com", password="abcd")
+        username = self.user_manager.available_username("empurrando juntos", "empurrando2@email.com")
+        #Last name
+        assert username =="juntos"
+
+        self.user_manager.create_user(username="juntos", email="empurrando2@email.com", password="abcd")
+        username = self.user_manager.available_username("empurrando juntos", "empurrando2@email.com")
+        #Last name + email domain
+        assert username == "juntos_email"
+
+        self.user_manager.create_user(username="juntos_email", email="empurrando2@email.com", password="abcd")
+        username = self.user_manager.available_username("empurrando juntos", "empurrando2@email.com")
+        #Emailname + email domain
+        assert username == "empurrando2_email"
+
+        self.user_manager.create_user(username="empurrando2_email", email="empurrando2@email.com", password="abcd")
+
+        #Test username generation for emailname + numbers from 0 to 1000
+        for i in range(1000):
+            username = self.user_manager.available_username("empurrando juntos", "empurrando2@email.com")
+            assert username == "empurrando2_" + str(i)
+            self.user_manager.create_user(username="empurrando2_" + str(i), email="empurrando2@email.com", password="abcd")
+
+        #After 1000 name should be random
+        username = self.user_manager.available_username("empurrando juntos", "empurrando2@email.com")
+        assert username != "empurrando2_1001"
+
+
+
+
+
+
+
+
