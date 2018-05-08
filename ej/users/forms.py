07b0fc112f834c4e9068745609c879fd5104b355
pablodiegoss <pablodiegoss@hotmail.com>
2018-05-08 15:53:05 -0300

    Create tests for user forms

--- ej/users/forms.py
+++ ej/users/forms.py
@@ -8,7 +8,7 @@ class EJSignupForm(forms.Form):
 
     field_order = ['full_name', 'email', 'password1', 'password2', 'capcha']
 
-    def signup(self, request, user):
+    def signup(self, user):
         full_name = self.cleaned_data['full_name']
         user.first_name, _, user.last_name = full_name.partition(' ')
         user.save()
