3f4363338318bbaab84f824a7c799dfdecd1669b
rodrigocam <rodrigo.redcode@gmail.com>
2018-05-16 16:39:24 -0300

    Change tests to use pytest fixture and change method return

--- ej/configurations/tests/test_models.py
+++ ej/configurations/tests/test_models.py
@@ -1,28 +1,63 @@
-from test_plus.test import TestCase
+import pytest
+from django.db import IntegrityError
 
-from ej.configurations.models import SocialMediaIcon
+from ej.configurations.models import SocialMediaIcon, Color, Fragment
+from markupsafe import Markup
 
 
-class TestSocialMediaIcon(TestCase):
-
-    def setUp(self):
-        self.social_media_icon = SocialMediaIcon()
-        self.social_media_icon.social_network = 'twitter'
-
-    def test_str(self):
-        self.assertEqual(
-            str(self.social_media_icon),
-            'twitter'
+class TestSocialMediaIcon:
+    @pytest.fixture
+    def social_media_icon(self):
+        return SocialMediaIcon(
+            social_network='social network'
         )
 
-    def test_html(self):
-        self.assertEqual(
-            self.social_media_icon.__html__(),
-            '<a href=""><i  class="""></i></a>'
+    def test_social_media_icon_string_representation(self, social_media_icon):
+        assert str(social_media_icon) == 'social network'
+
+    def test_social_media_icon_conversion_to_html(self, social_media_icon):
+        assert social_media_icon.__html__() == '<a href=""><i  class="""></i></a>'
+
+    def test_social_media_icon_html_tag(self, social_media_icon):
+        assert social_media_icon.icon_tag() == '<i  class="""></i>'
+
+
+class TestColor:
+    @pytest.fixture
+    def color(self):
+        return Color(
+            name='red',
+            hex_value='#FF0000'
         )
 
-    def test_icon_tag(self):
-        self.assertEqual(
-            self.social_media_icon.icon_tag(),
-            '<i  class="""></i>'
+    def test_color_representation(self, color):
+        assert str(color) == 'red: #FF0000'
+
+
+class TestFragment:
+    @pytest.fixture
+    def fragment(self):
+        return Fragment(
+            name='name',
+            format='html',
+            content='content'
         )
+
+    @pytest.fixture
+    def fragment_db(self, fragment, db):
+        fragment.save()
+        return fragment
+
+    def test_fragment_conversion_to_html(self, fragment):
+        assert fragment.__html__() == Markup('<div>content</div>')
+
+        fragment.content = '<p>content</p>'
+        assert fragment.__html__() == Markup('<div><p>content</p></div>')
+
+    def test_fragment_string_representation(self, fragment):
+        assert str(fragment) == 'name'
+ 
+    def test_locked_fragment_cannot_be_deleted(self, fragment_db):
+        fragment_db.lock()
+        with pytest.raises(IntegrityError):
+            fragment_db.delete()
