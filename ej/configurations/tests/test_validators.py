aeee457e3fd94d214c3e414f06277476b629c7a5
rodrigocam <rodrigo.redcode@gmail.com>
2018-05-16 17:54:02 -0300

    Add tests for validators.py

---
+++ ej/configurations/tests/test_validators.py
@@ -0,0 +1,25 @@
+import pytest
+from django.core.exceptions import ValidationError
+from ej.configurations.validators import validate_icon_name
+
+INVALID_FA_ICON_NAME_MSG = [
+    'Invalid font awesome icon name. Please use the full format like '
+    'in "fab fa-facebook-f"'
+]
+
+INVALID_FA_ICON_MSG = [
+    'fa fa-test is an invalid Icones Font-awesome icon!'
+]
+
+def test_invalid_fa_icon_name():
+    try:
+        validate_icon_name('invalid_icon_name')
+    except ValidationError as e:
+        assert e.messages == INVALID_FA_ICON_NAME_MSG
+
+
+def test_non_existent_fa_icon():
+    try:
+        validate_icon_name('fa fa-test')
+    except ValidationError as e:
+        assert e.messages == INVALID_FA_ICON_MSG
