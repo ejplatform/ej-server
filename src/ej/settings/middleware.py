4df3949d531f6ed37f1fee1b65dc067deabebac5
Luan Guimarães <luang@protonmail.ch>
2018-05-29 17:20:50 -0300

    [Rocketchat] fix django corshearders setup

--- src/ej/settings/middleware.py
+++ src/ej/settings/middleware.py
@@ -3,9 +3,15 @@ from boogie.configurations import MiddlewareConf as Base
 
 class MiddlewareConf(Base):
     def get_middleware(self):
-        middleware = super().get_middleware()
+        middleware = [
+            'corsheaders.middleware.CorsMiddleware',
+            *super().get_middleware(),
+        ]
         if self.ENVIRONMENT == 'local':
-            middleware = ['debug_toolbar.middleware.DebugToolbarMiddleware', *middleware]
+            middleware = [
+                'debug_toolbar.middleware.DebugToolbarMiddleware',
+                *middleware
+            ]
         elif self.ENVIRONMENT == 'testing':
             middleware.remove('django.middleware.locale.LocaleMiddleware')
         return middleware
