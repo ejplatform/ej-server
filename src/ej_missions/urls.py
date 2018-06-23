fed92c98ebf32e34790de1d62c16fce9e7f181a2
David Carlos <ddavidcarlos1392@gmail.com>
2018-06-23 11:17:04 -0300

    Add endpoint to update a receipt status.

--- src/ej_missions/urls.py
+++ src/ej_missions/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'/(?P<mid>[0-9]+)/user-status/(?P<uid>[0-9]+)', views.MissionViewSet.as_view({'get': 'user_status'})),
     url(r'/(?P<pk>[0-9]+)/statistics', views.MissionViewSet.as_view({'get': 'statistics'})),
     url(r'/(?P<pk>[0-9]+)/receipt', views.MissionViewSet.as_view({'post': 'receipt'})),
+    url(r'/receipt/(?P<pk>[0-9]+)', views.MissionViewSet.as_view({'post': 'update_receipt'})),
     url(r'/(?P<pk>[0-9]+)', views.MissionViewSet.as_view({'get': 'retrieve'})),
     url(r'/accept', views.MissionViewSet.as_view({'post': 'accept'})),
     url(r'/$', views.MissionViewSet.as_view({'post': 'create', 'get': 'list'}))
--- src/ej_missions/views.py
+++ src/ej_missions/views.py
@@ -58,6 +58,14 @@ class MissionViewSet(viewsets.ViewSet):
         serializer = serializers.MissionSerializer(mission)
         return Response(serializer.data)
 
+    def update_receipt(self, request, pk):
+        data = json.loads(request.body.decode("utf8"))
+        receipt = models.Receipt.objects.filter(id=pk)[0]
+        receipt.status = data["status"]
+        receipt.save()
+        serializer = serializers.MissionReceiptSerializer(receipt)
+        return Response(serializer.data)
+
     def receipts(self, request, pk):
         queryset = models.Mission.objects.filter(id=pk)[0].receipt_set.all()
         serializer = serializers.MissionReceiptSerializer(queryset, many=True)
@@ -91,3 +99,4 @@ class MissionViewSet(viewsets.ViewSet):
              statistics["pending"] += 1
 
         return Response(statistics)
+
