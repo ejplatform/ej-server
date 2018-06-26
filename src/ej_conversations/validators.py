75c15c390e16ce89db9d6002f5940b50822f7238
rodrigocam <rodrigo.redcode@gmail.com>
2018-06-26 19:53:39 -0300

    Add board_name field in creation of first conversation

--- src/ej_conversations/validators.py
+++ src/ej_conversations/validators.py
@@ -1,5 +1,6 @@
 import re
 
+from django.urls import resolve, Resolver404
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
@@ -12,6 +13,11 @@ except ImportError:
 
 COLOR_RE = re.compile(r'^#[0-9A-Fa-f]{3,4}(?:[0-9A-Fa-f]{3,4})?$')
 
+# Accepted characters in valid urls
+URL_VALID_CHARACTERS = list(
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~:/?#[]@!$&'()*+,;="
+)
+
 
 def validate_color(color):
     if not COLOR_RE.fullmatch(color) and color not in COLOR_NAMES:
@@ -21,3 +27,14 @@ def validate_color(color):
 def is_not_empty(text):
     if not text.strip():
         raise ValidationError(_('Field cannot be empty!'))
+
+
+def validate_board_name(board_name):
+    for c in list(board_name):
+        if c not in URL_VALID_CHARACTERS:
+            raise ValidationError(_("'{char}' is an invalid character!", char=c))
+    try:
+        resolve(f'/{board_name}/')
+        raise ValidationError(_("'{board}' already in use!", board=board_name))
+    except Resolver404:
+        pass
