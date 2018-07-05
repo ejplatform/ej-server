7d99cb33f553a9fbeae7c1220dce4fff12df45db
David Carlos <ddavidcarlos1392@gmail.com>
2018-07-04 23:16:35 -0300

    Add receipt on django admin.

--- src/ej_missions/admin.py
+++ src/ej_missions/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Mission, Comment
+from .models import Mission, Comment, Receipt
 
 admin.site.register(Mission)
+admin.site.register(Receipt)
 admin.site.register(Comment)
