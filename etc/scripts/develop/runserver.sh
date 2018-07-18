0942be0ff0bc6780bdf416521d5ff6eeb9ef0fc0
pablodiegoss <pablodiegoss@hotmail.com>
2018-07-18 10:27:57 -0300

    Fix docker CSS build before running server

--- etc/scripts/develop/runserver.sh
+++ etc/scripts/develop/runserver.sh
@@ -8,6 +8,9 @@ printf "\n## Performing Migrations\n\n"
 
 python3 manage.py migrate
 
+printf "\n## Building css"
+sass /app/lib/scss/main.scss:/app/lib/assets/css/main.css
+
 printf "\n## Starting Server\n\n"
 
 python3 manage.py runserver_plus 0.0.0.0:8000
