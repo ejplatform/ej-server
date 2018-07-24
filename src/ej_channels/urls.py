3e644e68f277fcdcceab5483c59acb6645d1e5e3
Daniel Henrique <danielhmarinho@gmail.com>
2018-07-24 16:25:44 -0300

    Show channel and changes to add and remove user

--- src/ej_channels/urls.py
+++ src/ej_channels/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls import url
 
 app_name = "ej_channels"
 urlpatterns = [
-    url(r'add-to-channel', views.ChannelViewSet.as_view({'put': 'add_to_channel'})),
-    url(r'remove-from-channel', views.ChannelViewSet.as_view({'put': 'remove_from_channel'})),
+    url(r'(?P<pk>[0-9]+)/add-to-channel', views.ChannelViewSet.as_view({'put': 'add_to_channel'})),
+    url(r'(?P<pk>[0-9]+)/remove-from-channel', views.ChannelViewSet.as_view({'put': 'remove_from_channel'})),
+    url(r'(?P<pk>[0-9]+)', views.ChannelViewSet.as_view({'get': 'show'})),
     url(r'$', views.ChannelViewSet.as_view({'post': 'create', 'get': 'index'})),
 ]
--- src/ej_channels/views.py
+++ src/ej_channels/views.py
@@ -18,26 +18,30 @@ class ChannelViewSet(viewsets.ViewSet):
         serializer = serializers.ChannelSerializer(queryset, many=True)
         return Response(serializer.data)
 
+    def show(self, request, pk):
+        queryset = models.Channel.objects.get(id=pk)
+        serializer = serializers.ChannelSerializer(queryset)
+        return Response(serializer.data)
+
     def create(self, request):
         data = request.data
         channel = models.Channel(name=data["name"])
-
         channel.save()
         serializer = serializers.ChannelSerializer(channel)
         return Response(serializer.data)
         
-    def add_to_channel(self, request):
+    def add_to_channel(self, request, pk):
         data = request.data
         user = User.objects.filter(id=data["user_id"])[0]
-        channel = models.Channel.objects.filter(id=data["id"])[0]
+        channel = models.Channel.objects.get(id=pk)
         channel.users.add(user)
         serializer = serializers.ChannelSerializer(channel)
         return Response(serializer.data)
 
-    def remove_from_channel(self, request):
+    def remove_from_channel(self, request, pk):
         data = request.data
         user = User.objects.filter(id=data["user_id"])[0]
-        channel = models.Channel.objects.filter(id=data["id"])[0]
+        channel = models.Channel.objects.get(id=pk)
         channel.users.remove(user)
         serializer = serializers.ChannelSerializer(channel)
         return Response(serializer.data)
