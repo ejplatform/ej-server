1107046e67cb849deb71c414305e43b5148df93d
pablodiegoss <pablodiegoss@hotmail.com>
2018-07-25 11:28:31 -0300

    Add translations to docker develop to fix tests

--- etc/scripts/develop/runserver.sh
+++ etc/scripts/develop/runserver.sh
@@ -11,5 +11,5 @@ printf "\n## Building css"
 sass /app/lib/scss/maindefault.scss:/app/lib/assets/css/maindefault.css
 
 printf "\n## Starting Server\n\n"
-
+inv i18n -c
 python3 manage.py runserver_plus 0.0.0.0:8000
