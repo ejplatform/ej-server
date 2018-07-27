c6691384d7892ada1b2f96516c02ad99b67b3764
Fabíola Fleury <fabiola.mfleury@gmail.com>
2018-07-27 11:19:15 -0300

    Fixing user profile reference to page, updating user tests

--- src/ej_users/tests/test_routes.py
+++ src/ej_users/tests/test_routes.py
@@ -1,11 +1,14 @@
 import pytest
 from django.test import Client
-from django.http import HttpResponseRedirect
-from ej_users.models import User
+from django.http import HttpResponseRedirect, HttpResponseServerError
+from django.test import TestCase
+from django import forms
 
+from ej_users.models import User
+from ej_users.forms import RegistrationForm
 
 @pytest.fixture
-def logged_client():
+def logged_client(db):
     user = User.objects.create_user('name@server.com', '1234', name='name')
     client = Client()
     client.force_login(user)
@@ -19,29 +22,6 @@ def mk_user():
     yield user
     user.delete()
 
-class TestRegisterRoute:
-    def test_register_route(self, db):
-        client = Client()
-        response = client.post('/register/', data={'name': 'something'})
-        assert response.status_code == 200
-
-    def test_register_valid_fields(self, db):
-        client = Client()
-        response = client.post('/register/', data={
-            'name': "Turanga Leela",
-            'email': "leela@example.com",
-            'password': "pass123",
-            'password_confirm': 'pass123'
-        })
-        assert response.status_code == 302
-        assert isinstance(response, HttpResponseRedirect)
-        assert response.url == '/'
-
-    def test_register_logged_user_route(self, db, logged_client):
-        client = logged_client
-        response = client.post('/register/', data={'name': 'something'})
-        assert response.status_code == 200
-
 
 class TestLoginRoute:
     def test_login_route(self, db):
@@ -49,24 +29,73 @@ class TestLoginRoute:
         response = client.post('/login/', data={'email': 'email'})
         assert response.status_code == 200
 
-    def test_login_logged_user_route(self, db, logged_client):
+    def test_login_logged_user_route(self, logged_client):
         client = logged_client
         response = client.post('/login/', data={'name': 'something'})
-        assert response.status_code == 200
+        assert response.url == '/profile/'
 
     def test_login_user_route(self, db, mk_user):
         user = mk_user
         client = Client()
+        assert '_auth_user_id' not in client.session
         response = client.post('/login/', data={'email': 'user@user.com', 'password': '1234'})
+
         assert response.status_code == 302
         assert isinstance(response, HttpResponseRedirect)
         assert response.url == '/'
+        assert int(client.session['_auth_user_id']) == user.pk
+
 
 class TestLogoutRoute:
     def test_logout_anonymous_user_route(self):
         client = Client()
         response = client.post('/logout/')
         assert response.status_code == 302
+
+    def test_logout_with_get(self):
+        client = Client()
+        response = client.get('/logout/')
+
+        assert isinstance(response, HttpResponseServerError)
+        assert response.status_code == 500
+        assert response.content.decode("utf-8")  == 'cannot logout using a GET'
+
+    def test_logout_logged_user(self, logged_client):
+        client = logged_client
+        assert '_auth_user_id' in client.session
+        response = client.post('/logout/')
+        assert not '_auth_user_id' in client.session
+
+
+
+class TestRegisterRoute(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('name@server.com', '1234', name='name')
+        client = Client()
+        client.force_login(self.user)
+        self.logged_client = client
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_register_route(self):
+        client = Client()
+        response = client.post('/register/', data={'name': 'something'})
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_valid_fields(self):
+        client = Client()
+        assert '_auth_user_id' not in client.session
+        response = client.post('/register/', data={
+            'name': "Turanga Leela",
+            'email': "leela@example.com",
+            'password': "pass123",
+            'password_confirm': 'pass123'
+        }, follow=True)
+        user_pk = User.objects.get(email="leela@example.com").pk
+        self.assertEqual(int(client.session['_auth_user_id']), user_pk)
+        self.assertRedirects(response,'/conversations/',302,200)
 
 
 class TestRecoverPasswordRoute:
@@ -83,15 +112,8 @@ class TestResetPasswordRoute:
         assert response.status_code == 302
 
 
-class TestRemoveProfileRoute:
-    def test_anonymous_user_remove_profile(self, db):
-        client = Client()
-        response = client.post('/profile/remove/')
-        assert response.status_code == 302
-
-
 class TestFavoriteConversationRoute:
-    def test_anonymous_user_remove_profile(self, db):
+    def test_anonymous_user_favorite_conversations(self, db):
         client = Client()
-        response = client.post('/profile/remove/')
+        response = client.post('/profile/favorites/')
         assert response.status_code == 302
