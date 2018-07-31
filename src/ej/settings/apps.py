075f01fac867124207ad0417d009c20decd47165
pablodiegoss <pablodiegoss@hotmail.com>
2018-07-31 14:48:43 -0300

    Add django-extensions to installed apps

--- src/ej/settings/apps.py
+++ src/ej/settings/apps.py
@@ -25,7 +25,6 @@ class InstalledAppsConf(Base, EjOptions):
         'courier',
         'courier.pushnotifications',
         'courier.pushnotifications.providers.onesignal',
-
         # Third party apps
         'taggit',
         'rules',
@@ -49,6 +48,7 @@ class InstalledAppsConf(Base, EjOptions):
         'pinax.badges',
         'constance',
         'constance.backends.database',
+        'django_extensions',
     ]
 
     def get_django_contrib_apps(self):
