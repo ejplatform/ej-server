b70e364b9e9238d6b56553c8ffd23eb83ab59e03
David Carlos <ddavidcarlos1392@gmail.com>
2018-08-02 18:28:54 -0300

    Move required_trophies action to ej_users

--- src/ej_users/api.py
+++ src/ej_users/api.py
@@ -1,5 +1,6 @@
 from boogie.rest import rest_api
 from ej_users.models import User
+from ej_trophies.models import Trophy
 
 #
 # User extra actions and attributes
@@ -8,3 +9,12 @@ from ej_users.models import User
 @rest_api.action('ej_users.User')
 def profile(request, user):
     return User.get_profile(user)
+
+@rest_api.action('ej_users.User')
+def required_trophies(request, user):
+    # Use trophy key or id to return required trophies list.
+    key = request.query_params.get('trophy')
+    try:
+        return Trophy.objects.get(pk=key).required_trophies.all()
+    except ValueError:
+        return Trophy.objects.get(key=key).required_trophies.all()
