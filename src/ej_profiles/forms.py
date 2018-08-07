a399f8135f912752d34fcb2092c3aee459190640
MatheusRich <matheusrichardt@gmail.com>
2018-08-07 10:36:29 -0300

    Rendering date field as date input

--- src/ej_profiles/forms.py
+++ src/ej_profiles/forms.py
@@ -1,7 +1,10 @@
 from django.forms import ModelForm
+from django import forms
 
 from . import models
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class ProfileForm(ModelForm):
     """
@@ -17,6 +20,9 @@ class ProfileForm(ModelForm):
             'birth_date','occupation',
             'image'
         ]
+        widgets = {
+            'birth_date': DateInput()
+        }
 
 
 class ProfileImageForm(ModelForm):
