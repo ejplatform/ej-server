83238718dd4d6957889addbf19ebd83a0c4ec603
Fábio Mendes <fabiomacedomendes@gmail.com>
2018-08-08 12:12:07 -0300

    fix typo

--- tasks.py
+++ tasks.py
@@ -341,7 +341,7 @@ def i18n(ctx, compile=False, edit=False, lang='pt_BR', keep_pot=False):
         ctx.run(f'poedit locale/{lang}/LC_MESSAGES/django.po')
     elif compile:
         ctx.run(f'{python} etc/scripts/compilemessages.py')
-    else:g
+    else:
         print('Collecting messages')
         manage(ctx, 'makemessages', all=True, keep_pot=True)
 
