dfabd52f2d4d4412b2e8bcd5fefd3e7c91309b73
Fábio Mendes <fabiomacedomendes@gmail.com>
2018-08-09 19:01:00 -0300

    remove unecessary scripts

--- etc/scripts/base/run_celery.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# wait for RabbitMQ server to start
-sleep 10
-
-# run Celery worker for our project with Celery configurations
-printf "\n## Starting Celery\n\n"
-cd /app
-celery worker -A ej.math -l info
--- etc/scripts/base/start_flower.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-sleep 10
-
-printf "\n## Starting Flower\n\n"
-cd /app
-celery flower -A ej.math --address=0.0.0.0 --port=8001
