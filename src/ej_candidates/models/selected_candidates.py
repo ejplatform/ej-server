660acad1992fc459cc157eb93d7d9ef490bc7319
Daniel Henrique <danielhmarinho@gmail.com>
2018-08-10 18:17:10 -0300

    Sendo notifications after select candidate

--- src/ej_candidates/models/selected_candidates.py
+++ src/ej_candidates/models/selected_candidates.py
@@ -3,6 +3,8 @@ from ej_users.models import User
 from .candidate import Candidate
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from ej_messages.models import Message
+from ej_channels.models import Channel
 
 from boogie import rules
 from boogie.rest import rest_api
@@ -18,5 +20,10 @@ class SelectedCandidate(models.Model):
     candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE, null=True)
 
 @receiver(post_save, sender=SelectedCandidate)
-def notify_user(sender, instance, created, **kwargs):
-    pass
+def send_message(sender, instance, created, **kwargs):
+    if created:
+        user = instance.user
+        title = instance.candidate.name
+        url = instance.candidate.external_page
+        channel = Channel.objects.filter(owner=user, sort="selected")[0]
+        Message.objects.create(channel=channel, title=title, body=url)
