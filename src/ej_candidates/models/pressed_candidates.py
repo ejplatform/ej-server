d07caebb2b696b55fde00fc8691f01d3549e49ab
Daniel Henrique <danielhmarinho@gmail.com>
2018-08-13 19:16:41 -0300

    Send message to user on pressed

--- src/ej_candidates/models/pressed_candidates.py
+++ src/ej_candidates/models/pressed_candidates.py
@@ -1,6 +1,10 @@
 from django.db import models
 from ej_users.models import User
 from .candidate import Candidate
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from ej_messages.models import Message
+from ej_channels.models import Channel
 
 from boogie import rules
 from boogie.rest import rest_api
@@ -14,3 +18,17 @@ class PressedCandidate(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE, null=True)
+
+@receiver(post_save, sender=PressedCandidate)
+def send_message(sender, instance, created, **kwargs):
+    if created:
+        user = instance.user
+        title = instance.candidate.name
+        url = instance.candidate.site_url
+        try:
+            channel = Channel.objects.filter(owner=user, sort="press")[0]
+        except IndexError:
+            channel = Channel.objects.create(name="press channel", sort="press", owner=user)
+            channel.users.add(user)
+            channel.save()
+        Message.objects.create(channel=channel, title=title, body="")
