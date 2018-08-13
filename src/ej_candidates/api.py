2e98aca71a831972c6565786c9cd032cd65b783a
David Carlos <ddavidcarlos1392@gmail.com>
2018-08-13 15:45:26 -0300

    Add endpoint to unselect some candidate.

--- src/ej_candidates/api.py
+++ src/ej_candidates/api.py
@@ -1,7 +1,8 @@
 from boogie.rest import rest_api
+import json
+
 from .models.candidate import Candidate
 from .models.selected_candidates import SelectedCandidate
-
 #
 # User extra actions and attributes
 #
@@ -12,8 +13,11 @@ def candidates(request, user):
         .exclude(pressedcandidate__user_id=user.id)
     return querySet.exclude(ignoredcandidate__user_id=user.id)
 
-# boogie has a bug. The underscore from the decorated method
-# is not being replaced by a an hifen.Fabio will fix it.
 @rest_api.action('ej_users.User')
 def selected_candidates(request, user):
     return Candidate.objects.filter(selectedcandidate__user_id=user.id)
+
+@rest_api.action('ej_users.User', methods=['post'])
+def unselect_candidate(request, user):
+    candidate = json.loads(request.body.decode("utf8"))["candidate"]
+    SelectedCandidate.objects.get(candidate=candidate, user=user).delete()
