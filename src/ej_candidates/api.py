ea0d3687c4c5a3b19fbbad046ce6c1ae41dfe987
David Carlos <ddavidcarlos1392@gmail.com>
2018-08-14 17:33:56 -0300

    Filter candidates by name.

--- src/ej_candidates/api.py
+++ src/ej_candidates/api.py
@@ -7,11 +7,21 @@ from .models.selected_candidates import SelectedCandidate
 # User extra actions and attributes
 #
 
+def filter_by_name(querySet, filter):
+    return querySet.filter(name=filter.upper())
+
+def filter_candidates(querySet, filter):
+        return filter_by_name(querySet, filter)
+
 @rest_api.action('ej_users.User')
 def candidates(request, user):
     querySet = Candidate.objects.exclude(selectedcandidate__user_id=user.id)\
         .exclude(pressedcandidate__user_id=user.id)
-    return querySet.exclude(ignoredcandidate__user_id=user.id).order_by("-id")[:10]
+    filter = request.GET.get('filter_by_name')
+    if (filter):
+        return filter_candidates(querySet, filter)
+    else:
+        return querySet.order_by("-id")[:10]
 
 @rest_api.action('ej_users.User')
 def selected_candidates(request, user):
