82faa4ad77de1ee8a4740804bd3f60df4e5ddaa6
David Carlos <ddavidcarlos1392@gmail.com>
2018-08-14 13:30:44 -0300

    add display_name on admin

--- src/ej_users/admin.py
+++ src/ej_users/admin.py
@@ -7,9 +7,9 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(AuthUserAdmin):
-    list_display = ('name', 'email', 'is_superuser')
+    list_display = ('display_name', 'name', 'email', 'is_superuser')
     fieldsets = (
-        (None, {'fields': ('email', 'name', 'password')}),
+        (None, {'fields': ('display_name', 'email', 'name', 'password')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
