79236095c824122506cdeba1374350d459226b91
Daniel Henrique <danielhmarinho@gmail.com>
2018-08-21 19:31:18 -0300

    Add filter to adhered measures

--- src/ej_candidates/api.py
+++ src/ej_candidates/api.py
@@ -35,6 +35,13 @@ def filter_by_uf(querySet, filter):
             return filteredCandidates
         return []
 
+def filter_by_adhered(querySet, filter):
+    if(querySet):
+        filteredCandidates = querySet.filter(adhered_to_the_measures=filter.upper())
+        if(filteredCandidates):
+            return filteredCandidates
+        return []
+
 
 def filter_candidates(querySet, filters):
     if(filters["filter_by_uf"]):
@@ -45,6 +52,8 @@ def filter_candidates(querySet, filters):
         querySet = filter_by_party(querySet, filters["filter_by_party"])
     if(filters["filter_by_candidacy"]):
         querySet = filter_by_candidacy(querySet, filters["filter_by_candidacy"])
+    if(filters["filter_by_adhered"]):
+        querySet = filter_by_adhered(querySet, filters["filter_by_adhered"])
     return querySet
 
 def get_filters(request):
@@ -53,11 +62,13 @@ def get_filters(request):
     filters["filter_by_uf"] = request.get('filter_by_uf')
     filters["filter_by_party"] = request.get('filter_by_party')
     filters["filter_by_candidacy"] = request.get('filter_by_candidacy')
+    filters["filter_by_adhered"] = request.get('filter_by_adhered')
     return filters
 
 def valid_filters(filters):
     return filters["filter_by_name"] or filters["filter_by_uf"] or\
-        filters["filter_by_party"] or filters["filter_by_candidacy"]
+        filters["filter_by_party"] or filters["filter_by_candidacy"] or\
+        filters["filter_by_adhered"]
 
 @rest_api.action('ej_users.User')
 def candidates(request, user):
