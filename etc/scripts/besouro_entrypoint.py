183335f47e5f26772878979895a630a39e9bee92
David Carlos <ddavidcarlos1392@gmail.com>
2018-08-22 23:59:38 -0300

    Remove unused script.

--- etc/scripts/besouro_entrypoint.py
+++ etc/scripts/besouro_entrypoint.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from subprocess import call
+import sys
 
 API_TAG = sys.argv[0]
 ENVIRONMENT = sys.argv[1]
@@ -8,10 +9,10 @@ ENVIRONMENT = sys.argv[1]
 def start():
     call("git fetch origin %s" % API_TAG, shell=True)
     call("git reset --hard FETCH_HEAD", shell=True)
-    call(["sed", "-i", "s#ENVIRONMENT = 'local'#ENVIRONMENT = '%s'# src/ej/settings/__init__.py" % ENVIRONMENT])
-    call("pip install -r etc/requirements/production.txt")
+    call(["sed", "-i", "s#ENVIRONMENT = 'local'#ENVIRONMENT = '%s'# /ej-server/src/ej/settings/__init__.py" % ENVIRONMENT])
+    call("pip install -r /ej-server/etc/requirements/production.txt", shell=True)
     call("pip install invoke", shell=True)
-    call("VOLATILE_DEPENDENCIES_STRATEGY=unknown pip install -r etc/requirements/git-modules.txt", shell=True)
+    call("VOLATILE_DEPENDENCIES_STRATEGY=unknown pip install -r /ej-server/etc/requirements/git-modules.txt", shell=True)
     call("./manage.py makemigrations --merge --noinput", shell=True)
     call("./manage.py makemigrations", shell=True)
     call("./manage.py migrate", shell=True)
