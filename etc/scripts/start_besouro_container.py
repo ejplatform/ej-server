cbc1414a06a64cd0eaf65346c2717b27eb973fda
David Carlos <ddavidcarlos1392@gmail.com>
2018-08-22 23:33:58 -0300

    Minor fixes on entrypoint script.

---
+++ etc/scripts/start_besouro_container.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+from subprocess import call
+import sys
+
+API_TAG = sys.argv[0]
+ENVIRONMENT = sys.argv[1]
+
+def start():
+    call("git fetch origin %s" % API_TAG, shell=True)
+    call("git reset --hard FETCH_HEAD", shell=True)
+    call(["sed", "-i", "s#ENVIRONMENT = 'local'#ENVIRONMENT = '%s'# /ej-server/src/ej/settings/__init__.py" % ENVIRONMENT])
+    call("pip install -r /ej-server/etc/requirements/production.txt", shell=True)
+    call("pip install invoke", shell=True)
+    call("VOLATILE_DEPENDENCIES_STRATEGY=unknown pip install -r /ej-server/etc/requirements/git-modules.txt", shell=True)
+    call("./manage.py makemigrations --merge --noinput", shell=True)
+    call("./manage.py makemigrations", shell=True)
+    call("./manage.py migrate", shell=True)
+    call("./manage.py collectstatic", shell=True)
+    call("inv gunicorn", shell=True)
+
+if __name__ == '__main__':
+    start()
