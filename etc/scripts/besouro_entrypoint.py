1976ad40e29bfe46d7408211923be4a0bbef8e86
David Carlos <ddavidcarlos1392@gmail.com>
2018-08-23 12:33:45 -0300

    Fix mailgun api.

--- etc/scripts/besouro_entrypoint.py
+++ etc/scripts/besouro_entrypoint.py
@@ -10,7 +10,7 @@ def start():
     call("git fetch origin %s" % API_TAG, shell=True)
     call("git reset --hard FETCH_HEAD", shell=True)
     call(["sed", "-i", "s#ENVIRONMENT = 'local'#ENVIRONMENT = '%s'#" % ENVIRONMENT,  "/ej-server/src/ej/settings/__init__.py"])
-    call(["sed", "-i", "s#'MAILGUN_API_KEY': ''#'MAILGUN_API_KEY': '707cd161665327e0378975e37972307-770f03c4-814b27bd'#", "/ej-server/src/ej/settings/__init__.py"])
+    call(["sed", "-i", "s#'MAILGUN_API_KEY': ''#'MAILGUN_API_KEY': 'd707cd161665327e0378975e37972307-770f03c4-814b27bd'#", "/ej-server/src/ej/settings/__init__.py"])
     call("pip install -r /ej-server/etc/requirements/production.txt", shell=True)
     call("pip install invoke", shell=True)
     call("VOLATILE_DEPENDENCIES_STRATEGY=unknown pip install -r /ej-server/etc/requirements/git-modules.txt", shell=True)
