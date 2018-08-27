856b1a8ad0fc706a26aae2c1516b97e990d96614
David Carlos <ddavidcarlos1392@gmail.com>
2018-08-27 10:36:08 -0300

    Filter candidates for unlogged users.

--- src/ej_candidates/api.py
+++ src/ej_candidates/api.py
@@ -1,84 +1,31 @@
 from boogie.rest import rest_api
+from django.http import JsonResponse
 import json
 
 from .models.candidate import Candidate
 from .models.selected_candidates import SelectedCandidate
-#
-# User extra actions and attributes
-#
-
-def filter_by_name(querySet, filter):
-    if(querySet):
-        filteredCandidates = querySet.filter(name__contains=filter.upper())
-        if(filteredCandidates):
-            return filteredCandidates
-        return []
-
-def filter_by_party(querySet, filter):
-    if(querySet):
-        filteredCandidates = querySet.filter(party=filter.upper())
-        if(filteredCandidates):
-            return filteredCandidates
-        return []
-
-def filter_by_candidacy(querySet, filter):
-    if(querySet):
-        filteredCandidates = querySet.filter(candidacy=filter.upper())
-        if(filteredCandidates):
-            return filteredCandidates
-        return []
-
-def filter_by_uf(querySet, filter):
-    if(querySet):
-        filteredCandidates = querySet.filter(uf=filter.upper())
-        if(filteredCandidates):
-            return filteredCandidates
-        return []
-
-def filter_by_adhered(querySet, filter):
-    if(querySet):
-        filteredCandidates = querySet.filter(adhered_to_the_measures=filter.upper())
-        if(filteredCandidates):
-            return filteredCandidates
-        return []
-
-
-def filter_candidates(querySet, filters):
-    if(filters["filter_by_uf"]):
-        querySet = filter_by_uf(querySet, filters["filter_by_uf"])
-    if(filters["filter_by_name"]):
-        querySet = filter_by_name(querySet, filters["filter_by_name"])
-    if(filters["filter_by_party"]):
-        querySet = filter_by_party(querySet, filters["filter_by_party"])
-    if(filters["filter_by_candidacy"]):
-        querySet = filter_by_candidacy(querySet, filters["filter_by_candidacy"])
-    if(filters["filter_by_adhered"]):
-        querySet = filter_by_adhered(querySet, filters["filter_by_adhered"])
-    return querySet
-
-def get_filters(request):
-    filters = {}
-    filters["filter_by_name"] = request.get('filter_by_name')
-    filters["filter_by_uf"] = request.get('filter_by_uf')
-    filters["filter_by_party"] = request.get('filter_by_party')
-    filters["filter_by_candidacy"] = request.get('filter_by_candidacy')
-    filters["filter_by_adhered"] = request.get('filter_by_adhered')
-    return filters
-
-def valid_filters(filters):
-    return filters["filter_by_name"] or filters["filter_by_uf"] or\
-        filters["filter_by_party"] or filters["filter_by_candidacy"] or\
-        filters["filter_by_adhered"]
+from .filters import *
 
 @rest_api.action('ej_users.User')
 def candidates(request, user):
-    try:
-        limit = int(request.GET.get("limit"))
-    except:
-        limit = 10
+    limit = get_query_limit(request)
     querySet = Candidate.objects.exclude(selectedcandidate__user_id=user.id)\
         .exclude(pressedcandidate__user_id=user.id)\
         .exclude(ignoredcandidate__user_id=user.id)
+    filters = get_filters(request.GET)
+    if (valid_filters(filters)):
+        result = filter_candidates(querySet, filters);
+        if (result):
+            result.order_by("-id")[:limit]
+        else:
+            return []
+    else:
+        return querySet.order_by("-id")[:limit]
+
+@rest_api.action('ej_users.User')
+def selected_candidates(request, user):
+    limit = get_query_limit(request)
+    querySet = Candidate.objects.filter(selectedcandidate__user_id=user.id)
     filters = get_filters(request.GET)
     if (valid_filters(filters)):
         result = filter_candidates(querySet, filters);
@@ -88,19 +35,6 @@ def candidates(request, user):
             return []
     else:
         return querySet.order_by("-id")[:limit]
-
-@rest_api.action('ej_users.User')
-def selected_candidates(request, user):
-    querySet = Candidate.objects.filter(selectedcandidate__user_id=user.id)
-    filters = get_filters(request.GET)
-    if (valid_filters(filters)):
-        return filter_candidates(querySet, filters)
-    else:
-        try:
-            limit = int(request.GET.get("limit"))
-            return querySet.order_by("-id")[:limit]
-        except:
-            return querySet.order_by("-id")
 
 @rest_api.action('ej_users.User', methods=['post'])
 def unselect_candidate(request, user):
