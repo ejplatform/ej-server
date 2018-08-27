1d47e374ff43f4fa6f16d34f590aa6264c5ead62
Fábio Mendes <fabiomacedomendes@gmail.com>
2018-08-27 12:13:04 -0300

    fix lower navbar

--- lib/themes/cpa/settings.py
+++ lib/themes/cpa/settings.py
@@ -3,7 +3,6 @@ EJ_PAGE_TITLE = 'Plataforma CPA'
 EJ_REGISTER_TEXT = 'Não faz parte da Plataforma CPA?'
 EJ_EXCLUDE_PROFILE_FIELDS = ['political_activity']
 EJ_MAX_BOARD_NUMBER = 0
-EJ_THEME_SHOW_NAVBAR_ICONS = False
 EJ_STATE_MAX_LENGTH = 2
 EJ_STATE_CHOICES = (
     ('AC', 'Acre'),
