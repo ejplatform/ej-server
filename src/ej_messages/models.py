91cfd0e8502dc8e69116992ee46ea6578d5837ef
Daniel Henrique <danielhmarinho@gmail.com>
2018-08-29 14:47:55 -0300

    Send fcm message on admin message

--- src/ej_messages/models.py
+++ src/ej_messages/models.py
@@ -6,6 +6,8 @@ from django.urls import reverse
 
 from ej_users.models import User
 from ej_channels.models import Channel
+from push_notifications.models import APNSDevice, GCMDevice
+from ej_profiles.models import Setting
 
 class Message(models.Model):
 	title = models.CharField(max_length=100)
@@ -28,4 +30,18 @@ def generate_notifications(sender, instance, **kwargs):
 	channel = Channel.objects.get(id=channel_id)
 	for user in channel.users.all():
 		Notification.objects.create(receiver=user, channel=channel, message=instance)
-		
+
+@receiver(post_save, sender=Message)
+def send_admin_fcm_message(sender, instance, created, **kwargs):
+	if created:
+		channel_id = instance.channel.id
+		channel = Channel.objects.get(id=channel_id)
+		users_to_send = []
+		if channel.sort == 'admin':
+			for user in channel.users.all():
+				setting = Setting.objects.get(owner_id=user.id)
+				if (setting.admin_notifications == True):
+					users_to_send.append(user)
+			fcm_devices = GCMDevice.objects.filter(cloud_message_type="FCM", user__in=users_to_send)
+			fcm_devices.send_message("", extra={"title": instance.title, "body": instance.body,
+				"icon":"https://i.imgur.com/D1wzP69.png", "click_action": instance.link})
