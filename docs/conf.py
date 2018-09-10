e28d6e0e12e4f966a0317b108e46ef5809259515
ValessioBrito <contato@valessiobrito.com.br>
2018-09-10 14:23:36 -0300

    Add source main.css and theme base

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Push Together'
-copyright = """2017, Bruno"""
+copyright = """2018, EJ"""
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -90,7 +90,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -120,6 +120,10 @@ html_theme = 'default'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_context = {
+    'css_files': ['http://localhost:8000/static/css/main.css'],
+}
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
