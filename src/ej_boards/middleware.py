119b293588b46013d7d6bf497b78e0787ec72af2
rodrigocam <rodrigo.redcode@gmail.com>
2018-10-17 16:45:54 -0300

    Remove case sensitive slug on all board routes

--- src/ej_boards/middleware.py
+++ src/ej_boards/middleware.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import resolve
+from django.http import Http404
 
 from .models import Board
 
 
-def BoardFallbackMiddleware(get_response):  # noqa: N802
+def BoardFallbackMiddleware(get_response):  # noqa: N802, C901
     """
     Look for board urls after 404 errors.
     """
@@ -18,11 +20,27 @@ def BoardFallbackMiddleware(get_response):  # noqa: N802
 
         # noinspection PyBroadException
         try:
-            slug = slugify(request.path.strip('/'))
+            slug = slugify(request.path.split('/')[1])
+
             if '/' in slug:
                 return response
+
             board = Board.objects.get(slug=slug)
-            return view_function(request, board=board)
+
+            # make a url with the real board slug e.g.: /Slug/edit/ becomes /slug/edit/
+            new_path = board.get_absolute_url() + '/'.join(request.path.split('/')[2:])
+
+            try:
+                view, args, kwargs = resolve(new_path)
+                new_response = view(request, **kwargs)
+                return new_response
+            except Http404:
+                # accessing /board-slug/
+                if '/' not in request.path.strip('/'):
+                    return view_function(request, board=board)
+
+                return response
+
         except Board.DoesNotExist:
             return response
         except Exception:
