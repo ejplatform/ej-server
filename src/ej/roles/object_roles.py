9cf70b470d0ee472f7f6bc7da558b2fc32254b95
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2018-10-25 10:08:31 -0300

    Support for collapsible menu UI

--- src/ej/roles/object_roles.py
+++ src/ej/roles/object_roles.py
@@ -1,7 +1,5 @@
-import uuid
-
 from django.db.models import QuerySet, Model
-from hyperpython import html, div, h2, span, Text, script
+from hyperpython import html, div, h2, span, Text
 from hyperpython.components import fa_icon, html_list
 
 
@@ -29,23 +27,11 @@ def render_collapsible(lst, item_role='collapsible-item', title=None, expanded=F
             raise TypeError('must provide an explicit title!')
 
     data = [html(x, item_role, **kwargs) for x in lst]
-    random_id = str(uuid.uuid4())
-    display = 'block' if expanded else 'none'
-    toggle_icon_function = script()[Text(f"""
-    up.on('click', '#{random_id+'h2'}', function() {{
-        $('#{random_id+'icon'}').toggleClass('fa-angle-down');
-        $('#{random_id+'icon'}').toggleClass('fa-angle-up');
-        $('#{random_id}').toggle();
-    }})
-    """, escape=False)]
-
-    return div(class_='CollapsibleList')[
-        h2(
-            id=random_id + "h2",
-            children=[title, span(f'({len(data)})'), fa_icon('angle-down', id=random_id + "icon")]
-        ),
-        toggle_icon_function,
-        html_list(data, style=f'display: {display}', id=random_id),
+    return div(class_='CollapsibleList', is_component=True)[
+        h2([title, span(f'({len(data)})'), fa_icon('angle-up')]),
+        div(class_='CollapsibleList-data')[
+            html_list(data),
+        ]
     ]
 
 
