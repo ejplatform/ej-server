ec61444f9471ba0f1641ec53382324260e856823
Gabriela Alves <gabrielaalvesdagama@gmail.com>
2018-11-01 11:58:19 -0300

    Inicial commit

--- src/ej_conversations/routes/comments.py
+++ src/ej_conversations/routes/comments.py
@@ -6,13 +6,18 @@ from . import urlpatterns, conversation_url
 
 
 @urlpatterns.route(conversation_url + 'comments/')
-def comment_list(request, conversation):
+def comment_list(request, conversation, comment):
     if not conversation.is_promoted:
         raise Http404
 
     user = request.user
     comments = conversation.comments.filter(author=user)
     n_comments = rules.compute('ej_conversations.remaining_comments', conversation, user)
+    # rejection_reason = comment.rejection_reason
+    # if rejection_reason in dict(Comment.REJECTION_REASON) and comment.status == comment.STATUS.rejected:
+    #     rejection_reason = dict(Comment.REJECTION_REASON)[comment.rejection_reason]
+    # else:
+    #     rejection_reason = None
     return {
         'content_title': _('List conversations'),
         'conversation': conversation,
@@ -22,6 +27,9 @@ def comment_list(request, conversation):
         'remaining_comments': n_comments,
         'can_comment': user.has_perm('ej.can_comment', conversation),
         'can_edit': user.has_perm('ej.can_edit_conversation', conversation),
+        'status': comment.status,
+        'status_name': dict(models.Comment.STATUS)[comment.status].capitalize(),
+        'rejection_reason': rejection_reason,
     }
 
 
