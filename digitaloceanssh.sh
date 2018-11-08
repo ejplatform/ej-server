c2a2595c5c60d18d41e2b491f5dd088e31004dc1
Leandro Nunes dos Santos <leandronunes@gmail.com>
2018-11-08 11:12:57 -0300

    make some improvements in deocker deployment

--- digitaloceanssh.sh
+++ digitaloceanssh.sh
@@ -1,7 +1,8 @@
-sshpass  -p $1 ssh -o StrictHostKeyChecking=no root@$2 <<-'ENDSSH'   
-    cd ej-server/docker/
+sshpass  ssh -o StrictHostKeyChecking=no $1@$2 <<-'ENDSSH'   
+    cd /app/docker/
     docker-compose -f docker-compose.deploy.yml down
     docker rmi $(docker images -q) -f
     docker network prune -f
     docker-compose -f docker-compose.deploy.yml up -d
+    docker-compose -f docker-compose.rocket.yml up -d
 ENDSSH
