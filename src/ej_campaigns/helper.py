7672c0fd6031296df643ce10169f8ebd526b267c
David Carlos <ddavidcarlos1392@gmail.com>
2019-06-06 14:41:28 -0300

    Add helper module to increase manutenability

---
+++ src/ej_campaigns/helper.py
@@ -0,0 +1,94 @@
+from django.db import transaction
+from django.http import HttpResponse, Http404
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+from jinja2 import Environment, FileSystemLoader
+import os
+
+from boogie.router import Router
+from ej_boards.models import Board, BoardSubscription
+from ej_conversations.models import Conversation
+
+existent_templates = ['mautic', 'hotsite']
+
+inlinePalettes = {
+    'green': ['#36C273', '#B4FDD4'],
+    'grey': ['#666666', '#EEEEEE'],
+    'blue': ['#30BFD3', '#C4F2F4'],
+    'orange': ['#F5700A', '#FFE1CA'],
+    'purple': ['#7758B3', '#E7DBFF'],
+    'pink': ['#C6027B', '#FFE3EA']
+}
+
+def paletteMixin(palette):
+    colors = inlinePalettes[palette]
+    templatePalette = {}
+    templatePalette['light'] = 'color: {}; background-color: {};'.format(colors[0], colors[1])
+    templatePalette['dark'] = 'color: {} !important; background-color: {};'.format(colors[1], colors[0])
+    templatePalette['arrow'] = 'border-top: 28px solid {} !important'.format(colors[1])
+    return templatePalette
+
+
+def templatePalette(conversation):
+    try:
+        currentPalette = BoardSubscription.objects.get(
+            conversation=conversation.id
+        ).board.palette.lower()
+    except:
+        currentPalette = 'blue'
+    paletteStyle = paletteMixin(currentPalette)
+    return paletteStyle
+
+def vote_url(request, conversation):
+    scheme = request.META['wsgi.url_scheme']
+    host = request.META['HTTP_HOST']
+    site_url = '{}://{}'.format(scheme,host)
+    board_slug = None
+    _vote_url = None
+    conversation_slug=conversation.slug
+    comment_id=conversation.comments.all()[0].id
+    try:
+        board_slug = BoardSubscription.objects.get(
+            conversation=conversation.id
+        ).board.slug
+        _vote_url = '{}/{}/conversations/{}?comment_id={}&action=vote'.format(
+            site_url,
+            board_slug,
+            conversation_slug,
+            comment_id
+        )
+    except:
+        _vote_url = '{}/conversations/{}?comment_id={}&action=vote'.format(
+            site_url,
+            conversation_slug,
+            comment_id
+        )
+    return _vote_url
+
+def generate_template_with_jinja(request, conversation, template_type):
+    root = os.path.dirname(os.path.abspath(__file__))
+    templates_dir = os.path.join(root, 'templates')
+    env = Environment(loader = FileSystemLoader(templates_dir))
+    template = env.get_template('{}.html'.format(template_type))
+    data = template.render(
+        conversation_title=conversation.text,
+        comment_content=conversation.comments.all()[0].content,
+        comment_author=conversation.comments.all()[0].author,
+        vote_url=vote_url(request,conversation),
+        tags=conversation.tags.all(),
+        paletteStyle=templatePalette(conversation)
+    )
+    return data
+
+def template_generator(request, conversation):
+    template_type = request.GET.get('type')
+    if (template_type and template_type in existent_templates):
+        template = generate_template_with_jinja(
+            request, conversation, template_type
+        )
+    else:
+        template = generate_template_with_jinja(
+            request, conversation, 'mautic'
+        )
+    return template
