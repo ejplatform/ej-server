73f29f88c262879c54525eb7371bf7b3334f189e
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2019-06-05 22:08:40 -0300

    Clean files from gh-pages

--- configure.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -z $VIRTUAL_ENV  && !($* == *--skip-venv*) ]]; then
-    echo "ERROR: configuration should run inside a virtual env"
-    echo "If you have virtualenvwrapper installed, run 'mkvirtualenv ej -p /usr/bin/python3'"
-    exit
-fi
-pip install invoke toml
-inv -e configure --silent --dev
