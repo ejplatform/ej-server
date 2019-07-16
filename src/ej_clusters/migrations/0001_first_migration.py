553593e8da83476fa4f733994854e333a2687cf2
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2019-07-16 17:36:25 -0300

    Fix broken imports on old migrations

--- src/ej_clusters/migrations/0001_first_migration.py
+++ src/ej_clusters/migrations/0001_first_migration.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
 import django.utils.timezone
-import ej_clusters.types
+import ej_clusters.enums
 import ej_conversations.models.vote
 import model_utils.fields
 
@@ -39,7 +39,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, editable=False, verbose_name='created')),
                 ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, editable=False, verbose_name='modified')),
-                ('cluster_status', boogie.fields.enum_field.EnumField(ej_clusters.types.ClusterStatus, default=ej_clusters.types.ClusterStatus(0))),
+                ('cluster_status', boogie.fields.enum_field.EnumField(ej_clusters.enums.ClusterStatus, default=ej_clusters.enums.ClusterStatus(0))),
                 ('unprocessed_votes', models.PositiveSmallIntegerField(default=0, editable=False)),
                 ('unprocessed_comments', models.PositiveSmallIntegerField(default=0, editable=False)),
                 ('conversation', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='clusterization', to='ej_conversations.Conversation')),
