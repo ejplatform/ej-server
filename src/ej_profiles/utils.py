124aca8a7eff776851bf311c1bcd8b106f7b8884
Hiroshi <lucas.hh@hotmail.com>
2019-10-29 14:31:53 -0300

    add func to get the location from ip

--- src/ej_profiles/utils.py
+++ src/ej_profiles/utils.py
@@ -1,3 +1,5 @@
+import geocoder
+
 from django.utils import timezone
 
 
@@ -7,3 +9,12 @@ def years_from(date, now=None):
     if date.month > now.month or (date.month == now.month and date.day > now.day):
         return years - 1
     return years
+
+def get_loc(ip_adr):
+    """
+    Method to get a lat and log from an ip
+    """
+    location = geocoder.ip(ip_adr)
+    
+    print(location)
+    return(location)
