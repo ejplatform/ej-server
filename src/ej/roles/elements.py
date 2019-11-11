ebe6c27a18eadf784630314b21ddaadee87c1bae
joao4018 <joao4018@gmail.com>
2019-11-11 18:49:38 -0300

    Changing variables names/reusing code

--- src/ej/roles/elements.py
+++ src/ej/roles/elements.py
@@ -20,19 +20,17 @@ def paired_links(left, right, **kwargs):
     """
     Layout that places an element to the left and a second element to the right.
     """
-    link_a = left
-    link_b = right
-    return paired_layout(link_a, link_b, **kwargs)
+    return paired_layout(left, right, **kwargs)
 
 
 def command_bar(*actions, **kwargs):
     """
     Element that includes configuration links bellow the header bar.
     """
-    if len(actions) == 1:
+    action_number = len(actions)
+    if action_number == 1:
         return div(actions, **kwargs).add_class("CommandBar")
-    elif len(actions) == 2:
+    elif action_number == 2:
         return div(actions, **kwargs).add_class("CommandBar")
     else:
-        n = len(actions)
-        raise ValueError(f"cannot include more than 2 actions, got: {n}")
+        raise ValueError(f"cannot include more than 2 actions, got: {action_number}")
