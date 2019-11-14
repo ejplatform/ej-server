fcba330e21532d96b6bddaa6a4bd3af2214506c9
Guilherme-Aguiar <gui9627oli@gmail.com>
2019-11-14 10:46:15 -0300

    added method compositing refactoring

--- src/ej/settings/dramatiq.py
+++ src/ej/settings/dramatiq.py
@@ -9,46 +9,72 @@ class DramatiqConf(Conf):
     DRAMATIQ_BROKER_HOST = env("", name="{name}")
     DRAMATIQ_BROKER_PORT = env(0, name="{name}")
 
+    url = None
+    kind = None
+    host = None
+    port = None 
+    kwargs = None
+    broker = None
+
+    def get_dramatiq_attributes(self):
+        """
+        This method sets the url, kind, host and port attributes for the broker object
+        """ 
+        if self.DRAMATIQ_BROKER_URL:
+            self.url = self.DRAMATIQ_BROKER_URL.strip()
+            if self.url is None:
+                self.kind, self.host, self.port = "stub", None, None
+            else:
+                self.kind, _, self.url = self.url.partition("://")
+                self.host, _, self.port = self.url.partition(":")
+                self.host = self.host or None
+                self.port = int(self.port) if self.port else None
+        else:
+            self.kind = self.DRAMATIQ_BROKER_TYPE
+            self.host = self.DRAMATIQ_BROKER_HOST or None
+            self.port = self.DRAMATIQ_BROKER_PORT or None
+
+    def separate_non_null_args(self):
+        """
+        This method separates the non null arguments in the kwargs dict
+        """ 
+        self.kwargs = [("self.host", self.host), ("port", self.port)]
+        self.kwargs = {k: v for k, v in self.kwargs if v is not None}
+
+    def initialize_broker(self):
+        """
+        This method initializes the broker object
+        """ 
+        if self.kind == "stub":
+            from dramatiq.brokers.stub import StubBroker
+            self.broker = StubBroker()
+        elif self.kind == "redis":
+            from dramatiq.brokers.redis import RedisBroker
+            self.broker = RedisBroker(**self.kwargs)
+        elif self.kind == "rabbitmq":
+            from dramatiq.brokers.rabbitmq import RabbitmqBroker
+            self.broker = RabbitmqBroker(**self.kwargs)
+        else:
+            raise ValueError(f"invalid dramatiq broker: {self.kind}")
+
+    
+
     def get_dramatiq_broker_object(self):
         """
         This method initializes the broker object for Dramatiq and saves it in
         Django's settings.
         """
+        self.get_dramatiq_attributes()
 
-        if self.DRAMATIQ_BROKER_URL:
-            url = self.DRAMATIQ_BROKER_URL.strip()
-            if url is None:
-                kind, host, port = "stub", None, None
-            else:
-                kind, _, url = url.partition("://")
-                host, _, port = url.partition(":")
-                host = host or None
-                port = int(port) if port else None
-        else:
-            kind = self.DRAMATIQ_BROKER_TYPE
-            host = self.DRAMATIQ_BROKER_HOST or None
-            port = self.DRAMATIQ_BROKER_PORT or None
+        self.separate_non_null_args()
 
-        # Separate non-null args
-        kwargs = [("host", host), ("port", port)]
-        kwargs = {k: v for k, v in kwargs if v is not None}
-
-        # Initializes broker
-        if kind == "stub":
-            from dramatiq.brokers.stub import StubBroker
-
-            broker = StubBroker()
-        elif kind == "redis":
-            from dramatiq.brokers.redis import RedisBroker
-
-            broker = RedisBroker(**kwargs)
-        elif kind == "rabbitmq":
-            from dramatiq.brokers.rabbitmq import RabbitmqBroker
-
-            broker = RabbitmqBroker(**kwargs)
-        else:
-            raise ValueError(f"invalid dramatiq broker: {kind}")
+        self.initialize_broker()
 
         # Configure as default and exit
-        dramatiq.set_broker(broker)
-        return broker
+        dramatiq.set_broker(self.broker)
+
+        return self.broker
+
+        
+
+        
