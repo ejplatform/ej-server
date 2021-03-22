bb23e7cb6d0786a20fbb11e0c40b12375a0ec89f
David Carlos <ddavidcarlos1392@gmail.com>
2021-03-22 15:57:29 +0000

    Adds theme colors on opinion component page
      - closes #243

--- src/ej_conversations/tests/test_tools_component.py
+++ src/ej_conversations/tests/test_tools_component.py
@@ -3,44 +3,81 @@ from ej_conversations.mommy_recipes import ConversationRecipes
 
 from ej_conversations.tools.forms import ConversationComponentForm
 from ej_conversations.tools.routes import conversation_component
+from ej_conversations.tools.models import ConversationComponent
 
 ConversationRecipes.update_globals(globals())
+
+
+class TestConversationComponentModel:
+    def test_conversation_component_get_props_with_analytics_auth(self):
+        form = ConversationComponentForm({'authentication_type': "analytics", 'theme': "default"})
+        component = ConversationComponent(form)
+        props = component.get_props()
+        assert props == " authenticate-with=analytics"
+
+    def test_conversation_component_get_props_with_mautic_auth(self):
+        form = ConversationComponentForm({'authentication_type': "mautic", 'theme': "default"})
+        component = ConversationComponent(form)
+        props = component.get_props()
+        assert props == " authenticate-with=mautic"
+
+    def test_conversation_component_get_props_with_register_auth(self):
+        form = ConversationComponentForm({'authentication_type': "register", 'theme': "default"})
+        component = ConversationComponent(form)
+        props = component.get_props()
+        assert props == " authenticate-with=register"
+
+    def test_conversation_component_get_props_with_custom_theme(self):
+        form = ConversationComponentForm({'authentication_type': "mautic", 'theme': "votorantim"})
+        component = ConversationComponent(form)
+        props = component.get_props()
+        assert props == "theme=votorantim authenticate-with=mautic"
+
+    def test_conversation_component_get_props_with_empty_form(self):
+        form = ConversationComponentForm({'authentication_type': "", 'theme': ""})
+        component = ConversationComponent(form)
+        props = component.get_props()
+        assert props == "theme= authenticate-with=register"
+
 
 class TestConversationComponentForm:
     def test_conversation_component_valid_form(self):
         form = ConversationComponentForm({'authentication_type': "register", 'theme': "default"})
         assert form.is_valid()
 
+
 class TestComponentConversationRoute(ConversationRecipes):
-     def test_post_component_conversation_valid_form(self, db, mk_conversation, rf):
+    def test_post_component_conversation_valid_form(self, db, mk_conversation, rf):
         conversation = mk_conversation()
 
         request = rf.post(
-            conversation.get_absolute_url() + '/tools/component', {'authentication_type': "register", 'theme': "default"}
+            conversation.get_absolute_url(
+            ) + '/tools/component', {'authentication_type': "register", 'theme': "default"}
         )
         response = conversation_component(request, conversation, None)
         assert response['conversation_component'].get_props() == " authenticate-with=register"
         assert response['form'].is_valid()
         assert response['conversation'].id == conversation.id
 
-     def test_post_votorantim_conversation_valid_form(self, db, mk_conversation, rf):
+    def test_post_votorantim_conversation_valid_form(self, db, mk_conversation, rf):
         conversation = mk_conversation()
 
         request = rf.post(
-            conversation.get_absolute_url() + '/tools/rasa', {'authentication_type': "mautic", 'theme': "votorantim"}
+            conversation.get_absolute_url(
+            ) + '/tools/component', {'authentication_type': "mautic", 'theme': "votorantim"}
         )
         response = conversation_component(request, conversation, None)
-        assert response['conversation_component'].get_props() == 'theme=votorantim  authenticate-with=mautic'
+        assert response['conversation_component'].get_props() == 'theme=votorantim authenticate-with=mautic'
         assert response['form'].is_valid()
         assert response['conversation'].id == conversation.id
 
-     def test_post_icd_conversation_valid_form(self, db, mk_conversation, rf):
+    def test_post_icd_conversation_valid_form(self, db, mk_conversation, rf):
         conversation = mk_conversation()
 
         request = rf.post(
-            conversation.get_absolute_url() + '/tools/rasa', {'authentication_type': "analytics", 'theme': "icd"}
+            conversation.get_absolute_url() + '/tools/component', {'authentication_type': "analytics", 'theme': "icd"}
         )
         response = conversation_component(request, conversation, None)
-        assert response['conversation_component'].get_props() == 'theme=icd  authenticate-with=analytics'
+        assert response['conversation_component'].get_props() == 'theme=icd authenticate-with=analytics'
         assert response['form'].is_valid()
         assert response['conversation'].id == conversation.id
--- src/ej_conversations/tools/forms.py
+++ src/ej_conversations/tools/forms.py
@@ -4,47 +4,8 @@ from django.utils.translation import ugettext_lazy as _
 
 from ej_boards.forms import PaletteWidget
 from ej.forms import EjModelForm
-from .models import RasaConversation
+from .models import RasaConversation, ConversationComponent
 
-
-class ConversationComponent:
-    AUTH_TYPE_CHOICES = (
-        ("register", _("Register using name/email")),
-        ("mautic", _("Mautic")),
-        ("analytics", _("Analytics")),
-    )
-
-    AUTH_TOOLTIP_TEXTS = {
-        "register": _("User will use EJ platform interface, creating an account using personal data"),
-        "mautic": _("Uses a mautic campaign "),
-        "analytics": _("Uses analytics cookies allowing you to cross vote data with user browser data.")
-    }
-
-    THEME_CHOICES = (
-        ("default", _("Default")),
-        ("votorantim", _("Votorantim")),
-        ("icd", _("ICD")),
-    )
-
-    THEME_PALETTES = {
-        "default": ["#1D1088", "#F8127E"],
-        "votorantim": ["#04082D", "#F14236"],
-        "icd": ["#005BAA", "#F5821F"]
-    }
-
-    def __init__(self, form):
-        self.form = form
-
-    def get_props(self):
-        if self.form.is_valid():
-            result = ""
-
-            if not self.form.cleaned_data["theme"] == "default":
-                result = result + f"theme={self.form.cleaned_data['theme']} "
-            if self.form.cleaned_data["authentication_type"]:
-                result = result + f" authenticate-with={self.form.cleaned_data['authentication_type']}"
-            return result
-        return "authenticate-with=register"
 
 class AuthWidget(forms.RadioSelect):
     template_name = "ej_conversations_tools/includes/auth-select.jinja2"
@@ -53,6 +14,7 @@ class AuthWidget(forms.RadioSelect):
     def render(self, name, value, attrs=None, renderer=None):
         context = self.get_context(name, value, attrs)
         return self.renderer.render(context)
+
 
 class ConversationComponentForm(forms.Form):
     authentication_type = forms.ChoiceField(
@@ -64,6 +26,7 @@ class ConversationComponentForm(forms.Form):
         required=False, widget=PaletteWidget
     )
 
+
 class RasaConversationForm(EjModelForm):
     class Meta:
         model = RasaConversation
@@ -72,6 +35,6 @@ class RasaConversationForm(EjModelForm):
 
     def __init__(self, *args, conversation, **kwargs):
         kwargs.update(initial={
-        'conversation': conversation
+            'conversation': conversation
         })
         super(RasaConversationForm, self).__init__(*args, **kwargs)
