cc155eb0d8854621fd5678221e9aa1973b319cd1
David Carlos <ddavidcarlos1392@gmail.com>
2021-03-24 10:57:47 -0300

    Enables EJ api documentation

--- src/ej/settings/__init__.py
+++ src/ej/settings/__init__.py
@@ -88,6 +88,7 @@ class Conf(
         "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
         "PAGE_SIZE": 50,
         "DEFAULT_VERSION": "v1",
+        'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
     }
 
     # REST_AUTH_REGISTER_SERIALIZERS = {
