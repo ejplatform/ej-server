b57aef77554c5a0ac6485fcc17a51f3548fea2e7
David Carlos <ddavidcarlos1392@gmail.com>
2021-04-08 23:11:54 +0000

    Migrates EJ dependencies management to Poetry tool.

---
+++ docker/configure.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# load poetry virtualenv
+source $(poetry env info --path)/bin/activate
+
+# prepare database
+inv db
+inv db-assets
+
+# install js dependencies
+cd lib && npm i && inv build-assets
+
+# compile sass files
+inv sass
+
+# generate translations
+inv i18n --compile
+inv i18n
+
+# generates documentation
+inv docs
+
+# runs django collectstatic command
+inv collect
+
+# runs develop server
+inv run
