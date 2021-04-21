d6fcf26eb5dada7e1e25dd832282b65c0caa4cdf
David Carlos <ddavidcarlos1392@gmail.com>
2021-04-21 12:50:51 -0300

    Fixes translations compilation

--- docker/run_local_server.sh
+++ docker/run_local_server.sh
@@ -8,14 +8,14 @@ inv db
 inv db-assets
 
 # install js dependencies
-cd lib && npm i && inv build-assets
+(cd lib && npm i && inv build-assets)
 
 # compile sass files
 inv sass
 
 # generate translations
-inv i18n --compile
 inv i18n
+inv i18n --compile
 
 # generates documentation
 inv docs
