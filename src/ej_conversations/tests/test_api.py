18799f9a4968a1de0f29e2814ebc90b2806c274f
Shayane <shayaneliebe@gmail.com>
2021-09-08 17:47:24 +0000

    Add comments count to API 416

--- src/ej_conversations/tests/test_api.py
+++ src/ej_conversations/tests/test_api.py
@@ -1,9 +1,12 @@
-from ej_conversations.models import Conversation, Comment
-from .examples import COMMENT, CONVERSATION, VOTE, VOTES
+import pytest
 from rest_framework.test import APIClient
 from rest_framework.authtoken.models import Token
 from ej_boards.mommy_recipes import BoardRecipes
-import pytest
+from ej_conversations.models import Conversation, Comment
+from .examples import COMMENT, CONVERSATION, VOTE, VOTES
+from ej_conversations.models.util import vote_count, statistics_for_user, statistics
+from ej_conversations.mommy_recipes import ConversationRecipes
+from ej_conversations.enums import Choice
 
 BASE_URL = "/api/v1"
 
@@ -110,3 +113,87 @@ class TestPostRoutes:
         comment = Comment.objects.first()
         data = api.get(comments_path + f"{comment.id}/", **self.EXCLUDES)
         assert data == comment_data
+
+
+class TestConversartionStatistics(ConversationRecipes):
+    def test_vote_count_of_a_conversation(self, db, mk_conversation, mk_user):
+        conversation = mk_conversation()
+        vote_count_result = vote_count(conversation)
+        assert vote_count_result == 0
+
+        user = mk_user(email="user@domain.com")
+        comment = conversation.create_comment(user, "aa", status="approved", check_limits=False)
+        comment.vote(user, "agree")
+        vote_count_result = vote_count(conversation)
+        assert vote_count_result == 1
+
+    def test_vote_count_agree(self, db, mk_conversation, mk_user):
+        conversation = mk_conversation()
+        user = mk_user(email="user@domain.com")
+        vote_count_result = vote_count(conversation, Choice.AGREE)
+        assert vote_count_result == 0
+
+        comment = conversation.create_comment(user, "aa", status="approved", check_limits=False)
+        comment.vote(user, "agree")
+        vote_count_result = vote_count(conversation, Choice.AGREE)
+        assert vote_count_result == 1
+
+        other = mk_user(email="other@domain.com")
+        comment = conversation.create_comment(user, "ab", status="approved", check_limits=False)
+        comment.vote(other, "disagree")
+        vote_count_result = vote_count(conversation, Choice.AGREE)
+        assert vote_count_result == 1
+
+    def test_vote_count_disagree(self, db, mk_conversation, mk_user):
+        conversation = mk_conversation()
+        user = mk_user(email="user@domain.com")
+        vote_count_result = vote_count(conversation, Choice.DISAGREE)
+        assert vote_count_result == 0
+
+        comment = conversation.create_comment(user, "ac", status="approved", check_limits=False)
+        comment.vote(user, "disagree")
+        vote_count_result = vote_count(conversation, Choice.DISAGREE)
+        assert vote_count_result == 1
+
+    def test_vote_count_skip(self, db, mk_conversation, mk_user):
+        conversation = mk_conversation()
+        user = mk_user(email="user@domain.com")
+        vote_count_result = vote_count(conversation, Choice.SKIP)
+        assert vote_count_result == 0
+
+        comment = conversation.create_comment(user, "ad", status="approved", check_limits=False)
+        comment.vote(user, "skip")
+        vote_count_result = vote_count(conversation, Choice.SKIP)
+        assert vote_count_result == 1
+
+    def test_statistics_return(self, db, mk_conversation):
+        conversation = mk_conversation()
+        statistics_result = statistics(conversation)
+
+        assert "votes" in statistics_result
+        assert "agree" in statistics_result["votes"]
+        assert "disagree" in statistics_result["votes"]
+        assert "skip" in statistics_result["votes"]
+        assert "total" in statistics_result["votes"]
+
+        assert "comments" in statistics_result
+        assert "approved" in statistics_result["comments"]
+        assert "rejected" in statistics_result["comments"]
+        assert "pending" in statistics_result["comments"]
+        assert "total" in statistics_result["comments"]
+
+        assert "participants" in statistics_result
+        assert "voters" in statistics_result["participants"]
+        assert "commenters" in statistics_result["participants"]
+        assert conversation._cached_statistics == statistics_result
+
+    def test_statistics_for_user(self, db, mk_conversation, mk_user):
+        conversation = mk_conversation()
+        user = mk_user(email="user@domain.com")
+        statistics_for_user_result = statistics_for_user(conversation, user)
+
+        assert "votes" in statistics_for_user_result
+        assert "missing_votes" in statistics_for_user_result
+        assert "participation_ratio" in statistics_for_user_result
+        assert "total_comments" in statistics_for_user_result
+        assert "comments" in statistics_for_user_result
